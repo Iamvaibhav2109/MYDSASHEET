14d518d6210bc8dda5cc5423c7fb0973c846187b
Iamvaibhav2109 <98706774+Iamvaibhav2109@users.noreply.github.com>
2024-07-09 21:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 39.5 MB (99.87%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int loopIteration=(2*n)-2;
+        int position=time%loopIteration;
+
+        if(position<n){
+            return position+1;
+        }
+        else{
+            return 2*n-position-1;
+        }  
+}
+}
