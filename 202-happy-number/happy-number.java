96e73d161c87e6ecc73f35656bffaf5536bcee6d
Iamvaibhav2109 <98706774+Iamvaibhav2109@users.noreply.github.com>
2024-07-09 20:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (65.52%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isHappy(int n) {
+        long div=0;
+        while(n!=1 && n!=4){
+            long num=n;
+            long sum=0;
+            while(num>0){
+               div=num%10;
+               sum+=Math.pow(div,2);  
+               num/=10; 
+            }
+            n=(int)sum; 
+          }
+        return n==1;
+        }
+    }
