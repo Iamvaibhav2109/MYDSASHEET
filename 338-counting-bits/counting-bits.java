45620001918d1d16957025f494ff9e27e015b360
Iamvaibhav2109 <98706774+Iamvaibhav2109@users.noreply.github.com>
2024-07-17 20:58:25 +0530

    Time: 2 ms (92.97%) | Memory: 46.5 MB (56.95%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -2,16 +2,8 @@ class Solution {
     public int[] countBits(int n) {
         int arr[]=new int[n+1];
         for(int i=0;i<=n;i++){
-            arr[i]=binaryNum(i);
+            arr[i]=arr[i>>1]+(i&1);
         }
         return arr;
-    }
-    public int binaryNum(int n){
-        int count=0;
-        while(n!=0){
-            count+=(n&1);
-            n=n>>1;
-        }
-        return count;
     }
 }
