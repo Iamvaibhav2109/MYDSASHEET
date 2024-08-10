b04f4dd6cc04f6b7f18b739553d78e1c423b8047
Iamvaibhav2109 <98706774+Iamvaibhav2109@users.noreply.github.com>
2024-08-10 13:43:21 +0530

    Time: 52 ms (28.33%) | Memory: 73.9 MB (19.69%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -3,6 +3,10 @@ class Solution {
         int count=0;
         int prev=0;
         Arrays.sort(intervals,Comparator.comparingInt(a->a[1]));
+        //This line sorts the intervals based on their ending times (a[1]). 
+        //The idea is that if you always choose the interval that ends the earliest,
+        // you'll have the most space left for the next intervals,
+        // potentially minimizing the number of overlapping intervals.
         for(int i=1;i<intervals.length;i++){
             if(intervals[prev][1]>intervals[i][0]){
                 count++;
